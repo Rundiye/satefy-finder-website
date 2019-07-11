33cb3e28e32c646ec0fc3ffa2826b18b3f6a3c92
Jundi Ye <rundiye93@gmail.com>
2019-07-11 13:15:01 +0200

    added styles

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -9,17 +9,15 @@ function LandingPage(parentElement) {
 LandingPage.prototype.generate = function() {
   this.elements = `
     <section class="landing-page">
-      <div class="one"><h1 class="welcome">! </h1></div>
-      <div class="two"><h2></h2></div>
-
       <div id="cf"class="three">
       <img class="travel-pic bottom" src="images/namibia.jpg" alt="travel pic">
       <img class="travel-pic top" src="images/vietnam.jpg" alt="travel pic">
      
       </div>
-      
+      <div class="botones">
       <div class="four"><button class="button-tips myButton"><a href="https://www.twowanderingsoles.com/blog/travel-safety-tips-you-need-to-know" target=_blank>Safety Tips</a></button></div>
       <div class="four"><button class="button-tips myButton"><a href="#0">Safetiest Countries</a></button></div>
+      </div>
       </section>
     
     
